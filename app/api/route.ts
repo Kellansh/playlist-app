200dbf65790cc3be09980c6e3c8b8f6ff21f0019
Kellansh <kellansh@gmail.com>
2024-06-22 15:05:29 -0400

    Proof of concept API route working

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,11 +1,10 @@
-// api > hello > route.ts
 import {NextRequest, NextResponse} from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 export async function GET (request: NextRequest){
-    const greeting = "Hello World!!"
-    const json = {
-        greeting
-    };
+    const json = await prisma.song.findMany();
     
     return NextResponse.json(json);
 }
